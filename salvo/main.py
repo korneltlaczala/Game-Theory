2f7a4a3f08ed105106fa15c357d39e511a1337a6
korneltlaczala <kornel.tlaczala@gmail.com>
2024-11-04 12:55:55 +0100

    strategy-groups

--- salvo/main.py
+++ salvo/main.py
@@ -1,13 +1,15 @@
 import numpy as np
 from itertools import combinations, permutations
 
-class Game:
+class SalvoGame:
 
     def __init__(self, board_size=3):
         self.board_size = board_size
         self.reset_board()
         self.generate_p1_strats()
         self.generate_p2_strats()
+        self.p1_strat_groups = []
+        self.p2_strat_groups = []
 
     def reset_board(self):
         self.board = [0 for i in range(self.board_size*self.board_size)]
@@ -36,9 +38,8 @@ class Game:
         self.p1_strats
 
     def generate_p2_strats(self):
-        # self.p2_strats = list(permutations(range(1, self.board_size + 1)))
         self.p2_strats = list(permutations(range(1, self.board_size*self.board_size + 1)))
-    
+
     def generate_payoff_matrix(self):
         self.payoff_matrix = []
         for p1_strat in self.p1_strats:
@@ -61,14 +62,52 @@ class Game:
         for i, row in enumerate(self.payoff_matrix):
             print(self.p1_strats[i], end="\t")
             print(np.mean(row))
+    
+class StrategyGroup():
+    def __init__(self, name="untitled"):
+        self.strategies = []
+        self.name = name
 
+    def add_strategy(self, strategy):
+        self.strategies.append(strategy)
+
+    def head(self, n=6):
+        outcome = ""
+        outcome += f"StrategyGroup {self.name}\n"
+        for i, strat in enumerate(self.strategies):
+            if i >= n:
+                break
+            outcome += f"{strat}\n"
+        outcome += "-----------------"
+        print(outcome)
+
+    def __str__(self):
+        outcome = ""
+        outcome += f"StrategyGroup {self.name}\n"
+        for strat in self.strategies:
+            outcome += f"{strat}\n"
+        outcome += "-----------------"
+        return outcome
 
 if __name__ == '__main__':
-    game = Game()
-    game.add_ship(5,6)
-    game.print_board()
-    print(game.p1_strats)
-    print(len(game.p2_strats))
+    game = SalvoGame()
+    p1_sg1 = StrategyGroup(name="[1,2]*")
+    p1_sg2 = StrategyGroup(name="[2,5]*")
 
-    game.generate_payoff_matrix()
-    game.display_payoff_matrix()
+    for strat in game.p1_strats:
+        if 5 not in strat:
+            p1_sg1.add_strategy(strat)
+        else:
+            p1_sg2.add_strategy(strat)
+
+    print(p1_sg1)
+    print(p1_sg2)
+
+    p2_sg1 = StrategyGroup(name="Center")
+    p2_sg2 = StrategyGroup(name="Border")
+    CENTER = {5}
+    BORDER = {2, 4, 6, 8}
+    CORNER = {1, 3, 7, 9}
+    for strat in game.p2_strats:
+        if strat[0] in CENTER and strat[1:5] in BORDER:
+            p2_sg1.add_strategy()
